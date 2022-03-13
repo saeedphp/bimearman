aaf1821b5297178e8d6728e8ca76bcfcf831cc6a
saeed <saeedkariminia542@gmail.com>
2022-03-13 17:13:17 +0330

    debug show more btn in reporting page

---
+++ components/reporting/report-item.js
@@ -0,0 +1,114 @@
+import {useMemo, useState} from "react";
+import styles from "./reporting.module.css";
+import Title from "../ui/title";
+import DownArrow from "../icons/down-arrow";
+import Card from "../ui/card";
+import CarInsurance from "../icons/insurance/car-insurance";
+
+const DUMMY_DATA = [
+    {
+        id: 'r1',
+        title: 'گزارشگری و افشای اطلاعات',
+        children: [
+            {
+                id: 'r1-1',
+                txt: 'مشخصات ثبتی شرکت',
+            },
+            {
+                id: 'r1-2',
+                txt: 'اساسنامه شرکت',
+            },
+            {
+                id: 'r1-3',
+                txt: 'مشخصات و اطلاعات مدیران شرکت',
+            },
+            {
+                id: 'r1-4',
+                txt: 'چشم انداز، ماموریت و اهداف راهبردی',
+            },
+            {
+                id: 'r1-5',
+                txt: 'طرح های جدید بیمه ای دارای مجوز از بیمه مرکزی',
+            },
+            {
+                id: 'r1-6',
+                txt: 'لیست ارزیابان خسارت بیمه (سامانه سنهاب)',
+            },
+            {
+                id: 'r1-7',
+                txt: 'اوراق مشارکت ، صکوک و سایر اوراق بهادار',
+            },
+            {
+                id: 'r1-8',
+                txt: 'اطلاعات تفکیکی نیروی انسانی',
+            },
+            {
+                id: 'r1-9',
+                txt: 'عملکرد بیمه گری مستقیم واتکائی به تفکیک رشته های بیمه ای',
+            }
+        ],
+    },
+];
+
+const ReportItem = () => {
+
+    const [items] = useState(DUMMY_DATA);
+    const [numberOfItemsShown, setNumberOfItemsToShown] = useState(4);
+
+    const showMore = () => {
+        if (numberOfItemsShown + 3 <= items.length) {
+            setNumberOfItemsToShown(numberOfItemsShown + 3);
+        } else {
+            setNumberOfItemsToShown(numberOfItemsShown + 3);
+        }
+    };
+
+    const itemsToShow = useMemo(() => {
+        return items
+            .map((report) => (
+                <li key={report.id}>
+                    <div className={styles.info}>
+                        <Title className={styles.title}>
+                            <i></i>
+                            {report.title}
+                            <DownArrow />
+                        </Title>
+                        <ul>
+                            {report.children.slice(0, numberOfItemsShown).map((values) => (
+                                <li key={values.id} className={styles.items}>
+                                    <div className={styles.tab}>
+                                  <span>
+                                      <i></i>
+                                  </span>
+                                        <Card className={styles.txt}>
+                                            {values.txt}
+                                        </Card>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <button className={styles['load-more']} onClick={() => {showMore(report.id)}}>
+                        مشاهده همه موارد
+                    </button>
+                </li>
+            ));
+    }, [items, numberOfItemsShown]);
+
+    return (
+      <>
+          <div className={styles.card}>
+              <ul className={styles.data}>
+                  {itemsToShow.length ? itemsToShow : "Loading..."}
+              </ul>
+              <div className={styles.img}>
+                  <span className={styles.icon}>
+                      <CarInsurance />
+                  </span>
+              </div>
+          </div>
+      </>
+    );
+};
+
+export default ReportItem;
